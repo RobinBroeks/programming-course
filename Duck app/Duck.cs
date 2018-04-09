2aab5be79174d8d36f06d28807d48fdb2f4b6ffe
broe0096 <broe0096@hz.nl>
2018-04-09 14:19:25 +0200

    first commit

---
+++ Duck app/Duck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duck_app
+{
+    public class Duck 
+    {
+        FlyBehavior flyBehavoir;
+        QuackBehavior quackBehavior;
+
+        public Duck()
+        {
+
+        }
+
+        public void display()
+        {
+
+        }
+
+        public void performFly()
+        {
+            flyBehavoir.fly();
+        }
+
+        public void performQuack()
+        {
+            quackBehavior.quack();
+        }
+
+        public void swim()
+        {
+            Console.WriteLine("all ducks float");
+        }
+    }
+}
---
+++ Duck app/MallardDuck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duck_app
+{
+    public class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+            QuackBehavior = new Quack();
+            flyBehavior = new FlyWithWings();
+        }
+
+        public void display()
+        {
+            Console.WriteLine();
+        }
+    }
+}
